22e926c7577247748cbc4b0cbe50eebb0614a94a
Devakumar MJ <devakumar.mj@gmail.com>
2023-07-10 19:48:49 +0530

    Movie names added

--- index.js
+++ index.js
@@ -176,3 +176,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+app.get('/', function (req, res) {
+    res.send('Please visit! React App');
+  });
